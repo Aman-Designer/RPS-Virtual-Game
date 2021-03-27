ac479e74548e17e6bf1c244a2ecee6fd52c68e9f
Aman-Designer <amankhristi@gmail.com>
2021-03-27 19:49:17 +0530

    Game Life Logic programmed

--- game.py
+++ game.py
@@ -4,6 +4,10 @@ t = ["Rock", "Paper", "Scissors"]
 
 computer = t[randint(0,2)]
 
+computer_lives = 3
+player_lives = 3
+total_lives =3
+
 player = False
 
 while player == False:
@@ -14,27 +18,49 @@ while player == False:
     print("Computer chose", computer)
 
     if player == computer:
-        print("Tie!")
+        print("Tie! No lives lost!")
     elif player == "Rock":
-
         if computer == "Paper":
-            print("I thought Tina never looses, but you lost from a computer!", computer, "covers", player)
+            print("I thought Tina never looses, but you lost from a computer!", computer, "covers", player,)
+            player_lives -= 1
+            print("Player Lives=", player_lives, "Computer Lives=", computer_lives)
         else:
             print("Tina wins!", player, "smashes", computer)
-    elif player == "Paper":
+            computer_lives -= 1
+            print("Player Lives=", player_lives, "Computer Lives=", computer_lives)
 
+    elif player == "Paper":
         if computer == "Scissors":
             print("Don't worry Tina, I'm sure you'll win next time.", computer, "cut", player)
+            player_lives -= 1
+            print("Player Lives=", player_lives, "Computer Lives=", computer_lives)
         else:
-            print("Tina wins!", player, "covers", computer)
-    elif player == "Scissors":
+            print("The almighty Tina wins!!", player, "covers", computer)
+            computer_lives -= 1
+            print("Player Lives=", player_lives, "Computer Lives=", computer_lives)
 
+    elif player == "Scissors":
         if computer == "Rock":
-            print("You're the Leonidas to the Sparta, GLORY!", computer, "smashes", player)
+            print("I'm sorry you have lost to a computer :(", computer, "smashes", player)
+            player_lives -= 1
+            print("Player Lives=", player_lives, "Computer Lives=", computer_lives)
         else:
-            print("Tina wins!", player, "cut", computer)
+            print("You're the Leonidas to the Sparta, GLORY!", player, "cut", computer)
+            computer_lives -= 1
+            print("Player Lives=", player_lives, "Computer Lives=", computer_lives)
+
     else:
         print("That's not a valid play. Check your spelling or Punctuation!")
 
-    player = False
+    if player_lives == 0:
+        player = True
+        print("You have faced your defeat")
+    else:
+        player = False
+
+    if computer_lives == 0:
+            player = True
+            print("The computer was bound to loose")
+    else:
+        player = False
     computer = t[randint(0,2)]
