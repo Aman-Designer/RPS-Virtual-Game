bd4c565cdc69ec987e4c4f3c5182f26d3c093e76
Aman-Designer <amankhristi@gmail.com>
2021-03-28 01:31:03 +0530

    Fuction deployed containing conditional statements

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 from random import randint
 
-from packagesMain import packageInit, functionMain
+from packagesMain import packageInit, functionMain, conditionalMain
 
 print("++++++ Welcome to the Legendary Rock Paper Scissors Game ++++++\n")
 
@@ -24,36 +24,7 @@ while packageInit.player == False:
     print("=========================")
     print("Computer chose", packageInit.computer, "\n")
 
-    if packageInit.player == packageInit.computer:
-        print("===================")
-        print("Tie! No lives lost!")
-        print("===================\n")
-    elif packageInit.player == "Rock":
-        if packageInit.computer == "Paper":
-            print("I thought Tina never looses, but you lost from a computer!", packageInit.computer, "covers", packageInit.player,"\n")
-            packageInit.player_lives -= 1
-        else:
-            print("Tina has won the great battel!", packageInit.player, "smashes", packageInit.computer, "\n")
-            packageInit.computer_lives -= 1
-
-    elif packageInit.player == "Paper":
-        if packageInit.computer == "Scissors":
-            print("Don't worry Tina, I'm sure you'll win next time.", packageInit.computer, "cut", packageInit.player, "\n")
-            packageInit.player_lives -= 1
-        else:
-            print("The almighty Tina wins!!", packageInit.player, "covers", packageInit.computer, "\n")
-            packageInit.computer_lives -= 1
-
-    elif packageInit.player == "Scissors":
-        if packageInit.computer == "Rock":
-            print("I'm sorry you have lost to a computer :(", packageInit.computer, "smashes", packageInit.player, "\n")
-            packageInit.player_lives -= 1
-        else:
-            print("You're the Leonidas to the Sparta, GLORY!", packageInit.player, "cut", packageInit.computer, "\n")
-            packageInit.computer_lives -= 1
-
-    else:
-        print("That's not a valid play. Check your spelling or Punctuation!")
+    conditionalMain.void(packageInit.player, packageInit.computer)
 
     if packageInit.player_lives == 0:
         functionMain.main("lost")
