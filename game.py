73c0ed3081f62e72e4219715131cbb4a98852190
Aman-Designer <amankhristi@gmail.com>
2021-03-28 00:37:43 +0530

    Package files added

--- game.py
+++ game.py
@@ -1,11 +1,6 @@
 from random import randint
 
-options = ["Rock", "Paper", "Scissors"]
-computer = options[randint(0,2)]
-computer_lives = 2
-player_lives = 2
-total_lives = 2
-player = False
+from packagesMain import packageInit
 
 def main(status):
     print("You have", status, "would you like to play again?")
@@ -19,67 +14,69 @@ def main(status):
         global computer_lives
         global total_lives
             
-        player_lives = total_lives
-        computer_lives = total_lives
+        packageInit.player_lives = packageInit.total_lives
+        packageInit.computer_lives = packageInit.total_lives
     else:
-        player = False
+        packageInit.player = False
 
 print("++++++ Welcome to the Legendary Rock Paper Scissors Game ++++++\n")
 
-while player == False:
+while packageInit.player == False:
 
     print("****************************************************************")
-    print("You have", player_lives, "left out of", total_lives)
-    print("The computer has", computer_lives, "left out of", computer_lives)
+    print("You have", packageInit.player_lives, "left out of", packageInit.total_lives)
+    print("The computer has", packageInit.computer_lives, "left out of", packageInit.computer_lives)
     print("****************************************************************\n")
     print("Choose from the options or enter q to exit!")
-    player = input("Rock, Paper, Scissors?\n")
+    packageInit.player = input("Rock, Paper, Scissors?\n")
 
-    if player == "q":
+    if packageInit.player == "q":
         print("You have chosen to quite the game! BYE!")
         print("===Goodluck in your future endeavors===")
         exit()
 
-    print("\nTina chose", player)
-    print("=========================")
-    print("Computer chose", computer, "\n")
+    packageInit.computer = packageInit.options[randint(0,2)]
 
-    if player == computer:
+    print("\nTina chose", packageInit.player)
+    print("=========================")
+    print("Computer chose", packageInit.computer, "\n")
+
+    if packageInit.player == packageInit.computer:
         print("===================")
         print("Tie! No lives lost!")
         print("===================\n")
-    elif player == "Rock":
-        if computer == "Paper":
-            print("I thought Tina never looses, but you lost from a computer!", computer, "covers", player,"\n")
-            player_lives -= 1
+    elif packageInit.player == "Rock":
+        if packageInit.computer == "Paper":
+            print("I thought Tina never looses, but you lost from a computer!", packageInit.computer, "covers", packageInit.player,"\n")
+            packageInit.player_lives -= 1
         else:
-            print("Tina has won the great battel!", player, "smashes", computer, "\n")
-            computer_lives -= 1
+            print("Tina has won the great battel!", packageInit.player, "smashes", packageInit.computer, "\n")
+            packageInit.computer_lives -= 1
 
-    elif player == "Paper":
-        if computer == "Scissors":
-            print("Don't worry Tina, I'm sure you'll win next time.", computer, "cut", player, "\n")
-            player_lives -= 1
+    elif packageInit.player == "Paper":
+        if packageInit.computer == "Scissors":
+            print("Don't worry Tina, I'm sure you'll win next time.", packageInit.computer, "cut", packageInit.player, "\n")
+            packageInit.player_lives -= 1
         else:
-            print("The almighty Tina wins!!", player, "covers", computer, "\n")
-            computer_lives -= 1
+            print("The almighty Tina wins!!", packageInit.player, "covers", packageInit.computer, "\n")
+            packageInit.computer_lives -= 1
 
-    elif player == "Scissors":
-        if computer == "Rock":
-            print("I'm sorry you have lost to a computer :(", computer, "smashes", player, "\n")
-            player_lives -= 1
+    elif packageInit.player == "Scissors":
+        if packageInit.computer == "Rock":
+            print("I'm sorry you have lost to a computer :(", packageInit.computer, "smashes", packageInit.player, "\n")
+            packageInit.player_lives -= 1
         else:
-            print("You're the Leonidas to the Sparta, GLORY!", player, "cut", computer, "\n")
-            computer_lives -= 1
+            print("You're the Leonidas to the Sparta, GLORY!", packageInit.player, "cut", packageInit.computer, "\n")
+            packageInit.computer_lives -= 1
 
     else:
         print("That's not a valid play. Check your spelling or Punctuation!")
 
-    if player_lives == 0:
+    if packageInit.player_lives == 0:
         main("lost")
 
-    if computer_lives == 0:
+    if packageInit.computer_lives == 0:
         main("won")
 
-    player = False
-    computer = options[randint(0,2)]
+    packageInit.player = False
+    computer = packageInit.options[randint(0,2)]
---
+++ packagesMain/packageInit.py
@@ -0,0 +1,5 @@
+options = ["Rock", "Paper", "Scissors"]
+computer_lives = 2
+player_lives = 2
+total_lives = 2
+player = False
